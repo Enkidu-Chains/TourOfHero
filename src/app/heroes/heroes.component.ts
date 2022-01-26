ce401bd5fb1f808a00c605bccad325c46eef17ec
Vlad Pyrch <vlad.nuron@gmail.com>
2022-01-26 21:21:08 +0200

    Add Service part 1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,44 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit
-{
-  private readonly _heroes: Hero[];
+export class HeroesComponent implements OnInit {
+  private _heroes?: Hero[];
 
   private _selectedHero?: Hero;
 
-  constructor()
-  {
-    this._heroes = HEROES;
+  constructor(private heroService: HeroService) {
   }
 
-  public get Heroes(): Hero[]
-  {
-    return [...this._heroes];
+  public get Heroes(): Hero[] {
+    return [...this._heroes ?? []];
   }
 
-  public get SelectedHero(): Hero | undefined
-  {
+  private set Heroes(heroes: Hero[]) {
+    this._heroes = heroes;
+  }
+
+  public get SelectedHero(): Hero | undefined {
     return this._selectedHero;
   }
 
-  private set SelectedHero(hero: Hero | undefined)
-  {
+  private set SelectedHero(hero: Hero | undefined) {
     this._selectedHero = hero;
   }
 
-  public OnSelect(hero: Hero): void
-  {
+  public OnSelect(hero: Hero): void {
     this.SelectedHero = hero;
   }
 
-  public ngOnInit(): void
-  {
+  public ngOnInit(): void {
+    this.InitHeroes();
+  }
+
+  private InitHeroes(): void {
+    this.heroService.GetHeroes()
+        .subscribe(heroes => this.Heroes = heroes);
   }
 }
